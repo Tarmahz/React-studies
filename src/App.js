1ebc8a7f365693c1ad830138d4c03d159f4be9e9
Mariam <mariamlawal165@gmail.com>
2022-08-30 14:37:01 +0100

    React praticals

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h2> My App </h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
